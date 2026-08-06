5a931efbe6f0512ec5785222f5ffd351c1bd0d12
Mai Xuân <nnmxuan23@clc.fitus.edu.vn>
2026-08-07 00:14:17 +0700

    chore

--- frontend/src/features/customer-commerce/pages/customer/PaymentResultPage.jsx
+++ frontend/src/features/customer-commerce/pages/customer/PaymentResultPage.jsx
@@ -20,8 +20,8 @@ import {
 import {
   finalizeVnpayReturn,
   finalizePaypalReturn,
-} from "../../../../shared/api/paymentApi";
-import { cancelOrder } from "../../../../shared/api/orderApi";
+} from "../../api/paymentApi";
+import { cancelOrder } from "../../api/orderApi";
 import { getVouchersByOrder } from "../../../../shared/api/issuedVoucherApi";
 import QrCodeDisplay from "../../../core-access/component/QRCodeDisplay";
 
