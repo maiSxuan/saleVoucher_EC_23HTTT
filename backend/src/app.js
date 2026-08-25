34e91c4d5e8efbbe01681173b2ce1d277c6c364e
Tramie_Nguyen <myn897676@gmail.com>
2026-08-26 00:16:40 +0700

    fix: CORS problem sending OTP by using email

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,7 @@ const { errorMiddleware } = require("./common/middleware/error.middleware");
 const config = loadEnvironment();
 
 const app = express();
-if (config.nodeEnv !== "production") {
-  app.use(cors(corsOptions));
-}
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
